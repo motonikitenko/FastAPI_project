37c1c21b0def820b6815e8a0a62c67b51622355a
motonikitenko <moto.nikitenko@gmail.com>
2026-05-15 19:20:26 +0300

    init

---
+++ app/main.py
@@ -0,0 +1,14 @@
+# main.py
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+class User(BaseModel):
+    username: str
+    message: str
+
+
+@app.post("/")
+async def root(user :User):
+    return user
